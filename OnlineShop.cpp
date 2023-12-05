e368ae60ba51bc56776f5d2cc7898e1eac5401be
Sten Lori <152862227+fluid-bipedal@users.noreply.github.com>
2023-12-05 13:31:27 +0600

    Create OnlineShop.cpp

---
+++ OnlineShop.cpp
@@ -0,0 +1,14 @@
+#include "OnlineShop.h"
+#include <iostream>
+
+void OnlineShop::run() {
+    // Implement the main logic of the online shop
+}
+
+void OnlineShop::displayProducts() const {
+    // Implement logic to display the available products
+}
+
+void OnlineShop::processOrder() {
+    // Implement order processing logic using the shopping cart
+}
