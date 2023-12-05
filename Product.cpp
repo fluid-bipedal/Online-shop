38ce84c5464e30fb21361dbba5948adcf3169d59
Sten Lori <152862227+fluid-bipedal@users.noreply.github.com>
2023-12-05 13:30:24 +0600

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,16 @@
+#include "Product.h"
+
+Product::Product(int id, const std::string& name, double price)
+    : id(id), name(name), price(price) {}
+
+int Product::getId() const {
+    return id;
+}
+
+const std::string& Product::getName() const {
+    return name;
+}
+
+double Product::getPrice() const {
+    return price;
+}
